9304d70ab110a0f13d74016ad832c0a9fa23566b
dbsgk <58998646+dbsgk@users.noreply.github.com>
2020-01-11 18:09:29 +0900

    Update DrCanvas.java

--- 14_swing/src/swing/DrCanvas.java
+++ 14_swing/src/swing/DrCanvas.java
@@ -1,7 +1,10 @@
 package swing;
 
 import java.awt.Canvas;
+import java.awt.Color;
 
 public class DrCanvas extends Canvas {
-
+	public DrCanvas() {
+		setBackground(new Color(220,180,255));	
+	}
 }
