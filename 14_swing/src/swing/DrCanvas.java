5f544330c9a02823de3692b134033045120c73f5
dbsgk <58998646+dbsgk@users.noreply.github.com>
2020-01-13 01:23:09 +0900

    Update DrCanvas.java

--- 14_swing/src/swing/DrCanvas.java
+++ 14_swing/src/swing/DrCanvas.java
@@ -2,9 +2,45 @@ package swing;
 
 import java.awt.Canvas;
 import java.awt.Color;
+import java.awt.Graphics;
 
 public class DrCanvas extends Canvas {
-	public DrCanvas() {
-		setBackground(new Color(220,180,255));	
+	private MsPaint mp;
+	public DrCanvas(MsPaint mp) {
+		this.mp = mp;
+		setBackground(new Color(220,180,255));
+		
 	}
+	@Override
+	public void paint(Graphics g) {
+		if (mp.getFill().isSelected()) {
+			setForeground(mp.col);
+	         if (mp.getLine().isSelected())
+	            g.drawLine(mp.x1, mp.y1, mp.x2, mp.y2);
+	         else if (mp.getCircle().isSelected())
+	            g.fillOval(mp.x1, mp.y1,  mp.x2-mp.x1, mp.y2-mp.y1);
+	         else if (mp.getRect().isSelected())
+	            g.fillRect(mp.x1, mp.y1, mp.x2-mp.x1, mp.y2-mp.y1);
+	         else if (mp.getRoundRect().isSelected())
+	            g.fillRoundRect(mp.x1, mp.y1, mp.x2-mp.x1, mp.y2-mp.y1, 50, 50);
+
+	      } else { 
+				setForeground(mp.col);
+	    	  if (mp.getLine().isSelected())
+		            g.drawLine(mp.x1, mp.y1, mp.x2, mp.y2);
+		         else if (mp.getCircle().isSelected())
+		            g.drawOval(mp.x1, mp.y1,  mp.x2-mp.x1, mp.y2-mp.y1);
+		         else if (mp.getRect().isSelected())
+		            g.drawRect(mp.x1, mp.y1, mp.x2-mp.x1, mp.y2-mp.y1);
+		         else if (mp.getRoundRect().isSelected())
+		            g.drawRoundRect(mp.x1, mp.y1, mp.x2-mp.x1, mp.y2-mp.y1, 50, 50);
+	      } 
+	
+		repaint();
+	}
+	public void update(Graphics g) {
+		paint(g);
+	}
+	
+	
 }
